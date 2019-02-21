6685acb295d9b564c2b09f8b52ca596dba909156
Matthew Buchanan <mdbuchanan@csustudent.net>
2019-02-21 02:52:29 +0000

    Change production webserver to use Puma

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   
   root 'static_pages#home'
+  
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
