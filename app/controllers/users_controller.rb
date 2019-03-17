8b67aa912159f7c4444d7ee4f9c28b834bf247b5
Matthew Buchanan <mdbuchanan@csustudent.net>
2019-03-17 02:50:31 +0000

    Implement User login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    if User.any?
+      redirect_to login_path
+    else
+    end
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Scheduler!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+ 
+  private
+
+    def user_params
+      params.require(:user).permit(:userName, :email, :password,
+                                   :password_confirmation)
+    end 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get help_url
+    get help_path
     assert_response :success
   end
 
