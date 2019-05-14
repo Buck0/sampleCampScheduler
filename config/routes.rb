2ed2e052c6ff597fbb9b88c44dd73670e03ea8ee
Matthew Buchanan <mdbuchanan@csustudent.net>
2019-05-14 21:02:00 +0000

    Commit to checkout new branch

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,11 @@ Rails.application.routes.draw do
   
   get '/admin', to: 'static_pages#admin'
   
-  root 'static_pages#admin'
+  #root 'static_pages#admin'
   
   get '/campers', to: 'campers#new'
   
-  #root 'static_pages#home'
+  root 'static_pages#home'
   
   get    '/new',     to: 'users#new'
   post   '/new',     to: 'users#create'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get root_path
+    get root_url
     assert_response :success
   end
 
   test "should get help" do
-    get help_path
+    get help_url
     assert_response :success
   end
 
